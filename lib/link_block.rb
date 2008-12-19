b065ee0fa44c9f674ff122d469e1bcbf20916dc4
Ryan Angilly <ryan@angilly.com>
2008-12-19 12:46:40 -0500

    writing tests on top of PlugTest

--- lib/link_block.rb
+++ lib/link_block.rb
@@ -1,5 +1,9 @@
-require 'link_block/config'
-require 'link_block/view_helpers'
+# The dirname is important, otherwise if rubygems is loaded before this,
+# (as is the case if you are using PlugTest), then the require will get
+# the installed gem instead of what you are developing.
+
+require File.dirname(__FILE__) + '/link_block/config'
+require File.dirname(__FILE__) + '/link_block/view_helpers'
 
 module LinkBlock
   def self.enable
---
+++ test/link_block_test.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/test_helper'
+require File.dirname(__FILE__) + '/../lib/link_block'
+
+class LinkBlockTest < Test::Unit::TestCase
+
+  def setup
+    pt_setup
+  end
+
+  def test_link_block_exists
+    # Make sure we're sane
+    assert @helper.respond_to?(:link_to)
+
+    # Make sure we're included
+    assert @helper.respond_to?(:link_block)
+  end
+
+  def test_link_block_yields_simple_link_tos
+    # Tried to make this a block... was taking too long.
+    res = pt_helper("link_to 'Sweet', :action => 'test'")
+
+    assert_select 'a', 'Sweet'
+    assert_match(/Sweet/, res)
+  end
+
+end
